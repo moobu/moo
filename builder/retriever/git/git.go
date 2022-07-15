defd2307b5f109bdf10cf75cecdde1cbb3fe57e9
Mivinci <1366723936@qq.com>
2022-07-16 01:05:13 +0800

    git retriever

--- builder/retriever/git/git.go
+++ builder/retriever/git/git.go
@@ -1,19 +1,52 @@
 package git
 
-import "github.com/moobu/moo/builder/retriever"
+import (
+	"fmt"
+	"math/rand"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
 
-// TODO: implement the git retriever
+	"github.com/moobu/moo/builder/retriever"
+)
+
 type git struct {
 	options retriever.Options
 }
 
-func (g *git) Retrieve(url string, opts ...retriever.RetrieveOption) (*retriever.Repository, error) {
+func (g *git) Retrieve(rawURL string, opts ...retriever.RetrieveOption) (*retriever.Repository, error) {
 	var options retriever.RetrieveOptions
 	for _, o := range opts {
 		o(&options)
 	}
 
-	return nil, nil
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+
+	name := u.Path[1:]
+	cmd := exec.Command("git", "clone", rawURL, name)
+	cmd.Dir = g.options.Dir
+	// TODO: open a new session logger for the output
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+
+	path := filepath.Join(g.options.Dir, name)
+	ref := options.Ref
+	if len(ref) > 0 {
+		// The hard man gave me a lot of lessons woo...
+		cmd := exec.Command("git", "reset", "--hard", ref)
+		cmd.Dir = path
+		if err := cmd.Run(); err != nil {
+			return nil, err
+		}
+	}
+
+	return &retriever.Repository{Path: path}, nil
 }
 
 func (g git) String() string {
@@ -26,4 +59,10 @@ func New(opts ...retriever.Option) retriever.Retriever {
 		o(&options)
 	}
 	return &git{options: options}
+}
+
+func rh4() string {
+	var buf [4]byte
+	rand.Read(buf[:])
+	return fmt.Sprintf("%x", buf)
 }
